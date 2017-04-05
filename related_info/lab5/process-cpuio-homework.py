ca0f62de0859acc6806c1bc52a666b0ce0ed8514
zhangxy <t_103z@yeah.net>
2017-04-05 16:13:19 +0800

    Lec 12 homework

--- related_info/lab5/process-cpuio-homework.py
+++ related_info/lab5/process-cpuio-homework.py
@@ -27,6 +27,7 @@ DO_COMPUTE = 'cpu'
 DO_YIELD = 'yld'
 DO_IO = 'io'
 
+
 class scheduler:
     def __init__(self, process_switch_behavior, io_done_behavior, io_length):
         # keep set of instructions for each of the processes
@@ -55,41 +56,58 @@ class scheduler:
             print '  and Z is the percent change that an instruction is IO'
             exit(1)
 
-        num_instructions, chance_yield, chance_io = int(tmp[0]), float(tmp[1])/100.0, float(tmp[2])/100.0
-        assert(chance_yield+chance_io<1)
+        num_instructions, chance_yield, chance_io = int(tmp[0]), float(tmp[1]) / 100.0, float(tmp[2]) / 100.0
+        assert (chance_yield + chance_io < 1)
 
-        #print "proc %d, num_instr %d, change_cpu %f" % (proc_id,num_instructions, chance_cpu)
+        # print "proc %d, num_instr %d, change_cpu %f" % (proc_id,num_instructions, chance_cpu)
         for i in range(num_instructions):
-            randnum=random.random();
-            if randnum < (1.0-chance_yield-chance_io):
+            randnum = random.random();
+            if randnum < (1.0 - chance_yield - chance_io):
                 self.proc_info[proc_id][PROC_CODE].append(DO_COMPUTE)
-            elif randnum >= (1.0-chance_yield-chance_io) and randnum < (1.0-chance_io):
+            elif randnum >= (1.0 - chance_yield - chance_io) and randnum < (1.0 - chance_io):
                 self.proc_info[proc_id][PROC_CODE].append(DO_YIELD)
             else:
                 self.proc_info[proc_id][PROC_CODE].append(DO_IO)
-            #print "proc %d, instr idx %d, instr cxt %s" % (proc_id, i, self.proc_info[proc_id][PROC_CODE][i])
+                # print "proc %d, instr idx %d, instr cxt %s" % (proc_id, i, self.proc_info[proc_id][PROC_CODE][i])
         return
 
-    #change to READY STATE, the current proc's state should be expected
-    #if pid==-1, then pid=self.curr_proc
+    # change to READY STATE, the current proc's state should be expected
+    # if pid==-1, then pid=self.curr_proc
     def move_to_ready(self, expected, pid=-1):
-        #YOUR CODE
-        return
+        if pid == -1:
+            pid = self.curr_proc
+        assert(self.proc_info[pid][PROC_STATE] == expected)
+        self.proc_info[pid][PROC_STATE] = STATE_READY
 
-    #change to RUNNING STATE, the current proc's state should be expected
+    # change to RUNNING STATE, the current proc's state should be expected
     def move_to_running(self, expected):
-        #YOUR CODE
-        return
+        assert(self.proc_info[self.curr_proc][PROC_STATE] == expected)
+        self.proc_info[self.curr_proc][PROC_STATE] = STATE_RUNNING
 
-    #change to DONE STATE, the current proc's state should be expected
+    # change to DONE STATE, the current proc's state should be expected
     def move_to_done(self, expected):
-        #YOUR CODE
-        return
+        assert(self.proc_info[self.curr_proc][PROC_STATE] == expected)
+        self.proc_info[self.curr_proc][PROC_STATE] = STATE_DONE
 
-    #choose next proc using FIFO/FCFS scheduling, If pid==-1, then pid=self.curr_proc
+    def move_to_wait(self, expected):
+        assert (self.proc_info[self.curr_proc][PROC_STATE] == expected)
+        self.proc_info[self.curr_proc][PROC_STATE] = STATE_WAIT
+
+    # choose next proc using FIFO/FCFS scheduling, If pid==-1, then pid=self.curr_proc
     def next_proc(self, pid=-1):
-        #YOUR CODE
-        return
+        if pid == -1:
+            pid = self.curr_proc
+        while True:
+            pid = (pid + 1) % self.get_num_processes()
+            if pid == self.curr_proc:
+                if self.proc_info[pid][PROC_STATE] == STATE_DONE or self.proc_info[pid][PROC_STATE] == STATE_WAIT:
+                    return
+                else:
+                    break
+            if self.proc_info[pid][PROC_STATE] == STATE_READY:
+                self.curr_proc = pid
+                break
+        self.move_to_running(STATE_READY)
 
     def get_num_processes(self):
         return len(self.proc_info)
@@ -111,7 +129,7 @@ class scheduler:
         num_active = 0
         for pid in range(len(self.proc_info)):
             if self.proc_info[pid][PROC_STATE] == STATE_READY or \
-                   self.proc_info[pid][PROC_STATE] == STATE_RUNNING:
+                            self.proc_info[pid][PROC_STATE] == STATE_RUNNING:
                 num_active += 1
         return num_active
 
@@ -122,7 +140,6 @@ class scheduler:
                 if t > current_time:
                     num_in_flight += 1
         return num_in_flight
-
 
     def space(self, num_columns):
         for i in range(num_columns):
@@ -151,7 +168,7 @@ class scheduler:
         self.move_to_running(STATE_READY)
 
         # OUTPUT: heade`[rs for each column
-        print '%s' % 'Time', 
+        print '%s' % 'Time',
         for pid in range(len(self.proc_info)):
             print '%10s' % ('PID:%2d' % (pid)),
         print '%10s' % 'CPU',
@@ -169,17 +186,16 @@ class scheduler:
             io_done = False
             for pid in range(len(self.proc_info)):
                 if clock_tick in self.io_finish_times[pid]:
-                    # if IO finished, the should do something for related process
-       	            #YOUR CODE
-                    pass #YOU should delete this
-            
+                    self.move_to_ready(STATE_WAIT, pid)
+                    io_done = True
+            if io_done == True and self.proc_info[self.curr_proc][PROC_STATE] != STATE_RUNNING:
+                self.next_proc()
+
             # if current proc is RUNNING and has an instruction, execute it
             instruction_to_execute = ''
             if self.proc_info[self.curr_proc][PROC_STATE] == STATE_RUNNING and \
-                   len(self.proc_info[self.curr_proc][PROC_CODE]) > 0:
-                #pop a instruction from proc_info[self.curr_proc][PROC_CODE]to instruction_to_execute
-                #YOUR CODE
-                pass #YOU should delete this
+                            len(self.proc_info[self.curr_proc][PROC_CODE]) > 0:
+                instruction_to_execute = self.proc_info[self.curr_proc][PROC_CODE].pop(0)
 
             # OUTPUT: print what everyone is up to
             if io_done:
@@ -188,7 +204,7 @@ class scheduler:
                 print '%3d ' % clock_tick,
             for pid in range(len(self.proc_info)):
                 if pid == self.curr_proc and instruction_to_execute != '':
-                    print '%10s' % ('RUN:'+instruction_to_execute),
+                    print '%10s' % ('RUN:' + instruction_to_execute),
                 else:
                     print '%10s' % (self.proc_info[pid][PROC_STATE]),
             if instruction_to_execute == '':
@@ -206,18 +222,21 @@ class scheduler:
             # if this is an YIELD instruction, switch to ready state
             # and add an io completion in the future
             if instruction_to_execute == DO_YIELD:
-                #YOUR CODE
-                pass #YOU should delete this
+                self.move_to_ready(STATE_RUNNING)
+                self.next_proc()
             # if this is an IO instruction, switch to waiting state
             # and add an io completion in the future
             elif instruction_to_execute == DO_IO:
-                #YOUR CODE
-                pass #YOU should delete this
+                self.move_to_wait(STATE_RUNNING)
+                self.io_finish_times[self.curr_proc].append(clock_tick + io_length)
+                self.next_proc()
+
 
             # ENDCASE: check if currently running thing is out of instructions
             self.check_if_done()
         return (cpu_busy, io_busy, clock_tick)
-        
+
+
 #
 # PARSE ARGUMENTS
 #
@@ -227,16 +246,18 @@ parser.add_option('-s', '--seed', default=0, help='the random seed', action='sto
 parser.add_option('-l', '--processlist', default='',
                   help='a comma-separated list of processes to run, in the form X1:Y1:Z1,X2:Y2:Z2,... where X is the number of instructions that process should run, and Y/Z the chances (from 0 to 100) issue an YIELD/IO',
                   action='store', type='string', dest='process_list')
-parser.add_option('-L', '--iolength', default=3, help='how long an IO takes', action='store', type='int', dest='io_length')
-parser.add_option('-p', '--printstats', help='print statistics at end; only useful with -c flag (otherwise stats are not printed)', action='store_true', default=False, dest='print_stats')
+parser.add_option('-L', '--iolength', default=3, help='how long an IO takes', action='store', type='int',
+                  dest='io_length')
+parser.add_option('-p', '--printstats',
+                  help='print statistics at end; only useful with -c flag (otherwise stats are not printed)',
+                  action='store_true', default=False, dest='print_stats')
 (options, args) = parser.parse_args()
 
 random.seed(options.seed)
 
 process_switch_behavior = SCHED_SWITCH_ON_IO
 io_done_behavior = IO_RUN_LATER
-io_length=options.io_length
-
+io_length = options.io_length
 
 s = scheduler(process_switch_behavior, io_done_behavior, io_length)
 
@@ -256,19 +277,19 @@ if process_switch_behavior == SCHED_SWITCH_ON_IO:
     print 'the current process is FINISHED or ISSUES AN YIELD or IO'
 else:
     print 'error in sched switch on iobehavior'
-    exit (-1)
+    exit(-1)
 print '  After IOs, the process issuing the IO will',
 if io_done_behavior == IO_RUN_LATER:
     print 'run LATER (when it is its turn)'
 else:
     print 'error in IO done behavior'
-    exit (-1)
+    exit(-1)
 print ''
 
 (cpu_busy, io_busy, clock_tick) = s.run()
 
 print ''
 print 'Stats: Total Time %d' % clock_tick
-print 'Stats: CPU Busy %d (%.2f%%)' % (cpu_busy, 100.0 * float(cpu_busy)/clock_tick)
-print 'Stats: IO Busy  %d (%.2f%%)' % (io_busy, 100.0 * float(io_busy)/clock_tick)
+print 'Stats: CPU Busy %d (%.2f%%)' % (cpu_busy, 100.0 * float(cpu_busy) / clock_tick)
+print 'Stats: IO Busy  %d (%.2f%%)' % (io_busy, 100.0 * float(io_busy) / clock_tick)
 print ''
